e389a00b285546ff216286ad6eb2354730ad29fb
rameen <141517153+rameennadeem12345@users.noreply.github.com>
2023-09-05 04:34:17 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+// create table of 5 by using loop
+var start = 1;
+while (start <= 10) {
+    let result = 5 * start;
+    console.log(" 5 * " + start + "=" + result);
+    start = start + 1;
+}
---
+++ index.ts
@@ -0,0 +1,10 @@
+// create table of 5 by using loop
+
+var start : number = 1;
+
+while(start <= 10) {
+let result = 5 * start;
+console .log(" 5 * " + start + "=" + result );
+
+start =start +1
+}
